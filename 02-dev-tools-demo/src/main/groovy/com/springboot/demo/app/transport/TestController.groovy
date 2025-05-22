57e393d704aa07c84ca3bdfd5441bbb8970ba16d
Michal Moudrý <moudry.michal@live.com>
2025-05-22 16:37:53 +0200

    updated gitignore + app props

--- 02-dev-tools-demo/src/main/groovy/com/springboot/demo/app/transport/TestController.groovy
+++ 02-dev-tools-demo/src/main/groovy/com/springboot/demo/app/transport/TestController.groovy
@@ -1,12 +1,16 @@
 package com.springboot.demo.app.transport
 
 import groovy.transform.CompileStatic
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @CompileStatic
 @RestController
 final class TestController {
+    @Value('${az.keyvault.connection}')
+    String keyVaultConnStr
+
     @GetMapping('/')
     String sayHello() {
         "Hello, world!"
@@ -20,5 +24,10 @@ final class TestController {
     @GetMapping('/amount')
     float getAmount() {
         5.3
+    }
+
+    @GetMapping('/kv')
+    String test() {
+        keyVaultConnStr
     }
 }
